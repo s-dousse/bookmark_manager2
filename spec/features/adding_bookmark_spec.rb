65a7715978b930e7608c4f2855dca7d420a399a9
Sarah <dousse.sarah@gmail.com>
2021-12-08 16:45:12 +0000

    10) create failing feature test : adding_bookmark_spec.rb

---
+++ spec/features/adding_bookmark_spec.rb
@@ -0,0 +1,16 @@
+require 'capybara/rspec'
+require_relative '../../app'
+Capybara.app = BookmarkManager
+
+feature 'Adding bookmarks' do
+  scenario 'A user can add bookmarks' do
+
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+
+    visit('/bookmarks')
+    fill_in('url', with: 'www.sarahdousse.com')
+    click_button('Save')
+    expect(page).to have_content('www.sarahdousse.com').once
+
+  end
+end
